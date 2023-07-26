fbf3f367a7f1eb8ceef9e86cc8f2f6600ff9ed05
mpatel34 <137090752+mpatel34@users.noreply.github.com>
2023-07-26 17:17:04 +0000

    all calculators working

--- app.rb
+++ app.rb
@@ -32,3 +32,48 @@ get ("/square/results") do
   @the_result = @the_num ** 2 
   erb (:square_results) 
 end
+
+
+get ("/square_root/new") do 
+  erb (:square_root)
+end
+
+get ("/square_root/results") do
+  @the_num = params.fetch("users_number").to_f
+
+  @the_result = Math.sqrt(@the_num)
+  erb(:square_root_results)
+end
+
+
+get ("/payment/new") do
+  erb(:payment)
+end
+
+get ("/payment/results") do
+  @the_apr = params.fetch("users_apr").to_f/100
+
+  @the_years = params.fetch("users_years").to_f
+
+  @the_principal = params.fetch("users_principal").to_f
+
+  @the_payment = @the_principal*((@the_apr*(1+@the_apr)**(@the_years*12))/((1+@the_apr)**(@the_years*12)-1))
+
+  erb(:payment_results)
+end
+
+
+get ("/random/new") do 
+  erb (:random_new)
+end
+
+get ("/random/results") do
+  @the_max = params.fetch("users_max").to_f
+
+  @the_min = params.fetch("users_min").to_f
+
+
+  @random_number = rand(@the_min..@the_max).to_f
+
+  erb(:random_results)
+end
